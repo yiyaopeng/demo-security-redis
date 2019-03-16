49e8a2e8acb2edbd7cddd417625c0a84d7e49dbd
simon <simon@yikb.com>
2019-03-16 21:54:54 +0800

    跳转

--- src/main/java/com/example/demo/RedisHttpSession.java
+++ src/main/java/com/example/demo/RedisHttpSession.java
@@ -5,12 +5,17 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.util.Properties;
 
 @EnableRedisHttpSession
 public class RedisHttpSession  extends WebSecurityConfigurerAdapter{
@@ -21,23 +26,27 @@ public class RedisHttpSession  extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http
-                .formLogin().loginPage("/loginPage").permitAll().successForwardUrl("/home")
+                .formLogin().successForwardUrl("/home")
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")).permitAll()
-                .and()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .csrf().disable();
-        http.addFilterBefore(new LoginAuthenticationFilter(customUserDetailsService(),authenticationManagerBean()), UsernamePasswordAuthenticationFilter.class);
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")).permitAll();
+//                .and()
+//                .authorizeRequests().anyRequest().authenticated()
+//                .and()
+//                .csrf().disable();
+//        http.addFilterAt(new LoginAuthenticationFilter(customUserDetailsService(),authenticationManagerBean()), UsernamePasswordAuthenticationFilter.class);
 
     }
 
 
-
+    @SuppressWarnings("deprecation")
     @Bean
-    public UserDetailsService customUserDetailsService() {
-        return new UserDetailsServiceImpl();
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
+//    @Bean
+//    public UserDetailsService customUserDetailsService() {
+//        return new UserDetailsServiceImpl();
+//    }
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory();
@@ -49,5 +58,16 @@ public class RedisHttpSession  extends WebSecurityConfigurerAdapter{
         return super.authenticationManagerBean();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        final Properties users = new Properties();
+        users.put("user","password,ROLE_USER,enabled"); //add whatever other user you need
+        return new InMemoryUserDetailsManager(users);
+    }
 
 }
