1e7c6ba71587665cc02ce3d23fceaed18a6880b0
simon <simon@yikb.com>
2019-03-16 22:20:45 +0800

    跳转

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
