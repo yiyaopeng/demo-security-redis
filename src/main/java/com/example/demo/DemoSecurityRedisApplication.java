26c038e6f4ae22b3d7166fdfc2d0e3f7415890fe
simon <simon@yikb.com>
2019-03-16 22:22:34 +0800

    跳转

--- src/main/java/com/example/demo/DemoSecurityRedisApplication.java
+++ src/main/java/com/example/demo/DemoSecurityRedisApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 public class DemoSecurityRedisApplication {
